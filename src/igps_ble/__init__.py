83a4a331d47564369f1a6ca1aa4c3db612c60013
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2026-05-31 15:52:12 -0300

    chore: project skeleton with bleak + pytest

---
+++ src/igps_ble/__init__.py
@@ -0,0 +1,7 @@
+"""Pure-Python BLE library for the iGPSPORT iGS10S cycling computer."""
+
+from .models import IGPSDeviceState
+
+__version__ = "0.1.0"
+
+__all__ = ["IGPSDeviceState", "__version__"]
